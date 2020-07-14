0a5bc80a7ee881242974cdb3f759d9312948d523
DimpalTodi <68284962+DimpalTodi@users.noreply.github.com>
2020-07-14 19:42:34 +0530

    add initial cv website files

--- index.js
+++ index.js
@@ -1,86 +1,32 @@
+function playGame(){
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
 
 
-//detecting button press
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
-var numberOfButtons = document.querySelectorAll(".drum").length;
-
-for( var i = 0; i < numberOfButtons; i++) {
-
-
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
-   var buttonInnerHtml = this.innerHTML;
-
-   makeSound(buttonInnerHtml);
-   buttonAnimation(buttonInnerHtml);
-
-});
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
 }
-
-
-//detecting key press
-
-document.addEventListener("keypress", function(event) {
-     makeSound(event.key);
-     buttonAnimation(event.key);
-});
-
-function makeSound(key){
-
-    switch (key) {
-
-      case "w":
-         var tom1 = new Audio("sounds/tom-1.mp3");
-         tom1.play();
-         break;
-
-      case "a":
-          var tom2 = new Audio("sounds/tom-2.mp3");
-          tom2.play();
-          break;
-
-      case "r":
-         var tom3 = new Audio("sounds/tom-3.mp3");
-         tom3.play();
-         break;
-
-      case "k":
-           var tom4 = new Audio("sounds/tom-4.mp3");
-           tom4.play();
-          break;
-
-      case "l" :
-          var kick = new Audio("sounds/kick.mp3");
-          kick.play();
-          break;
-
-
-      case "m":
-          var snare = new Audio("sounds/snare.mp3");
-          snare.play();
-          break;
-
-
-      case "p":
-          var crash = new Audio("sounds/crash.mp3");
-          crash.play();
-          break;
-
-
-      default:console.log(key);
-  }
-
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
-function buttonAnimation(currentKey){
-
-var activeButton = document.querySelector("." + currentKey);
-
-activeButton.classList.add("pressed");
-
-setTimeout(function(){
-  activeButton.classList.remove("pressed");
-}, 100);
-
-
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
 }
+}
+document.querySelector("button").addEventListener("click", playGame);
